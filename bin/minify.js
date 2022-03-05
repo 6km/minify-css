41681993250dc5fa64e37a86bc569a4d39809176
Mohammed <gsft22221@gmail.com>
2022-03-05 11:12:43 +0200

    Initial commit

---
+++ bin/minify.js
@@ -0,0 +1,42 @@
+#! /usr/bin/env node
+var shell = require('shelljs');
+var postcss = require('postcss');
+var cssnano = require('cssnano');
+var autoprefixer = require('autoprefixer');
+var litePreset = require('cssnano-preset-lite');
+var fs = require('fs');
+var chalk = require('chalk');
+var path = require('path');
+var preset = require('cssnano-preset-lite')({
+    discardComments: { removeAll: true },
+});
+
+var colormin = require('postcss-colormin');
+var gradients = require('postcss-minify-gradients');
+var normalizeUrl = require('postcss-normalize-url');
+
+var args = process.argv.slice(2)
+
+async function minify(filepath) {
+    var result = await postcss([
+        cssnano({
+            preset: preset
+        }),
+        colormin(),
+        gradients(),
+        autoprefixer(),
+        normalizeUrl({
+            removeSingleSlash: true
+        }),
+    ]).process(fs.readFileSync(filepath), { from: 'undefined' });
+
+    fs.writeFileSync(filepath, result.css, { encoding: 'utf-8' })
+}
+
+if (args[0] && fs.existsSync(args[0])) {
+    minify(path.resolve(args[0]));
+
+    console.log(`${chalk.greenBright("[MinifyCSS]")} Completed!`);
+} else {
+    console.log(`${chalk.redBright("[MinifyCSS]")} Please provide a path to the css file\n  ${chalk.gray("$")}${chalk.reset()} minify-css /path/to/index.css`)
+}
